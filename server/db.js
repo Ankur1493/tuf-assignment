4d5b0ab5fdcf538cd4cb1a016ce6d13b91aaefc7
Ankur1493 <ankursharma1493@gmail.com>
2024-03-18 16:06:00 +0530

    imported the prisma client

---
+++ server/db.js
@@ -0,0 +1,6 @@
+// script.js
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default prisma;
--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import prisma from "../db.js";
 
 dotenv.config();
 const app = express();
@@ -12,4 +13,18 @@ app.get("/", (_, res) => {
   res.send("Hello Take U Forward");
 })
 
-app.listen(8000);
+const startServer = async () => {
+  try {
+    // Connect to the database
+    await prisma.$connect();
+    console.log("Database connected successfully");
+
+    app.listen(8000, () => {
+      console.log("Server is running on http://localhost:8000");
+    });
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+
+startServer();
